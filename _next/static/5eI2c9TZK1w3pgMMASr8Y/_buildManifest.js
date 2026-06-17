912f21a1fd831886846fb33b56e2dc5d35d6e83c
신예은 <shin-yeeun@sin-yeeun-ui-MacBookAir.local>
2026-06-18 07:26:23 +0900

    Updates

--- _next/static/5eI2c9TZK1w3pgMMASr8Y/_buildManifest.js
+++ _next/static/5eI2c9TZK1w3pgMMASr8Y/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dallergut-microsite//_next/:path+",
+        "destination": "/dallergut-microsite/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
